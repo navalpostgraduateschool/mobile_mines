3ce77699c490425f64ae60167bee4e1430a1a5c3
informaton <hyattmoore@gmail.com>
2026-03-10 18:01:19 -0700

    Update SEStaticMine.m

--- classes/SEStaticMine.m
+++ classes/SEStaticMine.m
@@ -15,6 +15,9 @@ classdef SEStaticMine < SEMine
         function update(obj, dt, force, ships)
 
             % SMCC TODO: update position based on force and time step.
+            obj.pos_x = obj.pos_x + dt*(force(1)+obj.dx);
+            obj.pos_y = obj.pos_y + dt*(force(2)+obj.dy);
+            obj.pos_z = min(0,obj.pos_z + dt*(force(3)+obj.dz));
 
             update@SEMine(obj, dt, force, ships);
         end
